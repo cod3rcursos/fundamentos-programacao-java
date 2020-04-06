aa50acffd0bf0ead3146aede2d52b6819b8fa479
ebdonato <ebdonato@users.noreply.github.com>
2020-04-05 22:44:04 -0300

    Regra Ano Bissexto
    Inserida a regra completa de anos bissextos, conforme https://pt.wikipedia.org/wiki/Ano_bissexto

--- CursoJavaExercicios/src/controle/Exercicio2.java
+++ CursoJavaExercicios/src/controle/Exercicio2.java
@@ -12,9 +12,20 @@ public class Exercicio2 {
 		System.out.println("Digite o ano: ");
 		int ano = scanner.nextInt();
 
-		int bissexto = ano % 4;
+		/**
+		 * 1. De 4 em 4 anos é ano bissexto.
+		 * 2. De 100 em 100 anos não é ano bissexto.
+         * 3. De 400 em 400 anos é ano bissexto.
+         * 4. Prevalecem as últimas regras sobre as primeiras.
+         * 
+         * https://pt.wikipedia.org/wiki/Ano_bissexto
+		 */
+		
+		int bissextoTest1 = ano % 4;
+		int bissextoTest2 = ano % 100;
+		int bissextoTest3 = ano % 400;
 
-		if (bissexto == 0) {
+		if (bissextoTest3 == 0 || ( bissextoTest1 == 0 && bissextoTest2 != 0 ) ) {
 			System.out.println(ano + " é um ano bissexto");
 		} else {
 			System.out.println(ano + " não é um ano bissexto");
