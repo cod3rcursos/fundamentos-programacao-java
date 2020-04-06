68d9da5635547d7d2e3d0d912f4629865dd13ee5
ebdonato <ebdonato@users.noreply.github.com>
2020-04-05 22:47:26 -0300

    Revert "Regra Ano Bissexto"
    This reverts commit aa50acffd0bf0ead3146aede2d52b6819b8fa479.

--- CursoJavaExercicios/src/controle/Exercicio2.java
+++ CursoJavaExercicios/src/controle/Exercicio2.java
@@ -12,20 +12,9 @@ public class Exercicio2 {
 		System.out.println("Digite o ano: ");
 		int ano = scanner.nextInt();
 
-		/**
-		 * 1. De 4 em 4 anos é ano bissexto.
-		 * 2. De 100 em 100 anos não é ano bissexto.
-         * 3. De 400 em 400 anos é ano bissexto.
-         * 4. Prevalecem as últimas regras sobre as primeiras.
-         * 
-         * https://pt.wikipedia.org/wiki/Ano_bissexto
-		 */
-		
-		int bissextoTest1 = ano % 4;
-		int bissextoTest2 = ano % 100;
-		int bissextoTest3 = ano % 400;
+		int bissexto = ano % 4;
 
-		if (bissextoTest3 == 0 || ( bissextoTest1 == 0 && bissextoTest2 != 0 ) ) {
+		if (bissexto == 0) {
 			System.out.println(ano + " é um ano bissexto");
 		} else {
 			System.out.println(ano + " não é um ano bissexto");
