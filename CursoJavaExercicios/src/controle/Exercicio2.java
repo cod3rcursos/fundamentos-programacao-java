c2756849ec4fc3ea819eaeb96df37966e87f9e20
Lucas Galdino <lucasgaldino7@gmail.com>
2022-08-24 13:50:01 -0300

    Update Exercicio2.java

--- CursoJavaExercicios/src/controle/Exercicio2.java
+++ CursoJavaExercicios/src/controle/Exercicio2.java
@@ -12,9 +12,9 @@ public class Exercicio2 {
 		System.out.println("Digite o ano: ");
 		int ano = scanner.nextInt();
 
-		int bissexto = ano % 4 == 0 && ((ano % 100) != 0  || ano % 400 == 0);
+		boolean bissexto = ano % 4 == 0 && ((ano % 100) != 0  || ano % 400 == 0);
 
-		if (bissexto == 0) {
+		if (bissexto == true) {
 			System.out.println(ano + " é um ano bissexto");
 		} else {
 			System.out.println(ano + " não é um ano bissexto");
