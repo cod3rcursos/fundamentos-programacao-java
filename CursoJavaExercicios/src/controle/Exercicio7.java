09cd29aa97efadba80774240ac502bf733eae60b
Lucas Galdino <lucasgaldino7@gmail.com>
2023-01-18 11:45:51 -0300

    Update Exercicio7.java

--- CursoJavaExercicios/src/controle/Exercicio7.java
+++ CursoJavaExercicios/src/controle/Exercicio7.java
@@ -3,28 +3,27 @@ package controle;
 import java.util.Scanner;
 
 public class Exercicio7 {
-	/**
-	 * 7. Criar um programa que enquanto estiver recebendo números positivos,
-	 * imprime no console a soma dos números inseridos, caso receba um número
-	 * negativo, encerre o programa. Tente utilizar a estrutura do while.
-	 */
+ /**
+  * 7. Criar um programa que enquanto estiver recebendo números positivos,
+  * imprime no console a soma dos números inseridos, caso receba um número
+  * negativo, encerre o programa. Tente utilizar a estrutura do while.
+  */
 
-	public static void main(String[] args) {
+ public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
+  Scanner scanner = new Scanner(System.in);
 
-		int somadorDeNumeros = 0;
-		int numero = 0;
+  int somadorDeNumeros = 0;
+  int numero = 0;
 
-		while (numero >= 0) {
-			System.out.println("Digite um numero inteiro (zero ou negativo para sair): ");
-			numero = scanner.nextInt();
-			if (numero >= 0) {
-				somadorDeNumeros += numero; // Isso é igual a somadorDeNumeros = somadorDeNumeros + numero;
-				System.out.printf("\nSoma até o momento: %d\n", somadorDeNumeros);
-			}
-		}
-		scanner.close();
-	}
-
+  while (numero >= 0) {
+   System.out.println("Digite um numero inteiro (ou negativo para sair): ");
+   numero = scanner.nextInt();
+   if (numero >= 0) {
+    somadorDeNumeros += numero; // Isso é igual a somadorDeNumeros = somadorDeNumeros + numero;
+    System.out.printf("\nSoma até o momento: %d\n", somadorDeNumeros);
+   }
+  }
+  scanner.close();
+ }
 }
